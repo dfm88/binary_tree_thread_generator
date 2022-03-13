a217f48fbcda59098f9ec7edca0535d02ab51e65
dfm88 <diegof.university@gmail.com>
2022-03-13 18:30:03 +0100

    completed tree generation

--- main.c
+++ main.c
@@ -8,10 +8,54 @@ int maxDepth;
 
 typedef struct {
     int myDepth;
-    ulong* myAncestorsPidList;
+    ulong *myAncestorsPidList;
 } threadStruct;
 
-void* generateThreads(void* par){
+void *generateThreads(void *par) {
+
+    threadStruct *tSpoint, tSobj;
+    tSpoint = (threadStruct *) par; //take the struct from arg
+    tSobj = *tSpoint;
+    int newDepth = tSobj.myDepth +1;
+    pthread_t thread;
+
+    if (tSobj.myDepth < maxDepth) {
+        // create 2 new threads with increased depth
+        // and reference to parent Tid
+        threadStruct newTsList[2];
+
+        for (int i = 0; i < 2; i++) {
+            newTsList[i].myDepth = newDepth;
+            newTsList[i].myAncestorsPidList = malloc(
+                    sizeof(ulong) * newDepth
+            );
+            // copy all parent ids into mine, adding the current one
+            for (int j = 0; j < tSobj.myDepth; j++) {
+                newTsList[i].myAncestorsPidList[j] = tSobj.myAncestorsPidList[j];
+            }
+
+            newTsList[i].myAncestorsPidList[tSobj.myDepth] = pthread_self();
+
+            pthread_create(
+                    &thread,
+                    NULL,
+                    generateThreads,
+                    (void *) &newTsList[i]
+            );
+
+            pthread_join(thread, NULL);
+        }
+
+    } else {
+        // max Depth reached, end recursion
+        printf("Thread '%ld' 'tree: ", pthread_self());
+        for (int k = 0; k < maxDepth; k++) {
+            printf("%ld ", tSobj.myAncestorsPidList[k]);
+        }
+        putchar('\n');
+        free(tSobj.myAncestorsPidList);
+    }
+    pthread_exit(NULL);
 
 }
 
@@ -30,23 +74,31 @@ void* generateThreads(void* par){
 int main(int argc, char **argv) {
 
     maxDepth = atoi(argv[1]);
-    threadStruct tS[2];
+    threadStruct tsList[2];
     int currentDepth = 0;
     pthread_t thread;
 
     // create firsts two threads
-    for (int i = 0; i < 2; ++i) {
-        tS[i].myDepth = currentDepth+1;
+    for (int i = 0; i < 2; i++) {
+        int newDepth = currentDepth+1;
+        tsList[i].myDepth = newDepth;
         // dynamic array of thread pids
-        tS[i].myAncestorsPidList = (ulong *)malloc(
-                sizeof(ulong) * maxDepth
+        tsList[i].myAncestorsPidList = (ulong *) malloc(
+                sizeof(ulong) * newDepth
         );
         // assign to first element of array the root thread id
-        tS[i].myAncestorsPidList[currentDepth] = pthread_self();
-        pthread_create(&thread, NULL, generateThreads, (void *) &tS[i])
+        tsList[i].myAncestorsPidList[currentDepth] = pthread_self();
+        pthread_create(
+                &thread,
+                NULL,
+                generateThreads,
+                (void *) &tsList[i]
+        );
+//        pthread_join(thread, NULL);
     }
 
 
+    pthread_exit(NULL);
     return 0;
 
 }
